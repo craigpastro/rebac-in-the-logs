4aaed2b51e146ee13eca4f11cf2b6fbdd989b9eb
Craig Pastro <pastro.craig@gmail.com>
2022-12-22 11:41:48 -0800

    renaming

--- swipl/zanzibar.pl
+++ swipl/zanzibar.pl
@@ -7,29 +7,33 @@ check(Namespace, Id, Rel, User) :-
 
 list(Namespace, Id, Rel, User) :-
     config(Namespace, Rel, Rewrite),
-    checkWR(Namespace, Id, Rel, User, Rewrite).
+    listWR(Namespace, Id, Rel, User, Rewrite).
 
-% checkWR is check with rewrite
-checkWR(Namespace, Id, Rel, User, self) :- tuple(Namespace, Id, Rel, User).
+% listWR is list with rewrite
+listWR(Namespace, Id, Rel, User, self) :-
+    tuple(Namespace, Id, Rel, User).
 
-checkWR(Namespace, Id, _, User, computedUserset(Rel0)) :-
+listWR(Namespace, Id, _, User, computedUserset(Rel0)) :-
     list(Namespace, Id, Rel0, User).
 
-checkWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
+listWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
     tuple(Namespace, Id, Tupleset, object(Namespace0, Id0)),
     list(Namespace0, Id0, ComputedUserset, User).
 
-checkWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
+listWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
     tuple(Namespace, Id, Tupleset, userset(Namespace0, Id0, ComputedUserset)),
     list(Namespace0, Id0, ComputedUserset, User).
 
-checkWR(Namespace, Id, Rel, User, union(L, _)) :- checkWR(Namespace, Id, Rel, User, L).
-checkWR(Namespace, Id, Rel, User, union(_, R)) :- checkWR(Namespace, Id, Rel, User, R).
+listWR(Namespace, Id, Rel, User, union(L, _)) :-
+    listWR(Namespace, Id, Rel, User, L).
 
-checkWR(Namespace, Id, Rel, User, intersection(L, R)) :-
-    checkWR(Namespace, Id, Rel, User, L),
-    checkWR(Namespace, Id, Rel, User, R).
+listWR(Namespace, Id, Rel, User, union(_, R)) :-
+    listWR(Namespace, Id, Rel, User, R).
 
-checkWR(Namespace, Id, Rel, User, exclusion(M, S)) :-
-    checkWR(Namespace, Id, Rel, User, M),
-    \+ checkWR(Namespace, Id, Rel, User, S).
+listWR(Namespace, Id, Rel, User, intersection(L, R)) :-
+    listWR(Namespace, Id, Rel, User, L),
+    listWR(Namespace, Id, Rel, User, R).
+
+listWR(Namespace, Id, Rel, User, exclusion(M, S)) :-
+    listWR(Namespace, Id, Rel, User, M),
+    \+ listWR(Namespace, Id, Rel, User, S).
