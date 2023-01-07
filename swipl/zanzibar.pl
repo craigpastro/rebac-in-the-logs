a1bbd94be4e1f78e0eb004e4937ed823bf109569
Craig Pastro <pastro.craig@gmail.com>
2023-01-07 14:25:28 -0800

    Renamings (#10)

--- swipl/zanzibar.pl
+++ swipl/zanzibar.pl
@@ -2,38 +2,39 @@
 :- dynamic(tuple/4).
 
 % check add the cut at the end of list so it just finds an answer and won't backtrack.
-check(Namespace, Id, Rel, User) :-
-    list(Namespace, Id, Rel, User), !.
+check(Namespace, ObjectId, Relation, User) :-
+    list(Namespace, ObjectId, Relation, User),
+    !.
 
-list(Namespace, Id, Rel, User) :-
-    config(Namespace, Rel, Rewrite),
-    listWR(Namespace, Id, Rel, User, Rewrite).
+list(Namespace, ObjectId, Relation, User) :-
+    config(Namespace, Relation, Rewrite),
+    listWR(Namespace, ObjectId, Relation, User, Rewrite).
 
 % listWR is list with rewrite
-listWR(Namespace, Id, Rel, User, self) :-
-    tuple(Namespace, Id, Rel, User).
+listWR(Namespace, ObjectId, Relation, User, self) :-
+    tuple(Namespace, ObjectId, Relation, User).
 
-listWR(Namespace, Id, _, User, computedUserset(Rel0)) :-
-    list(Namespace, Id, Rel0, User).
+listWR(Namespace, ObjectId, _, User, computedUserset(Relation0)) :-
+    list(Namespace, ObjectId, Relation0, User).
 
-listWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
-    tuple(Namespace, Id, Tupleset, object(Namespace0, Id0)),
-    list(Namespace0, Id0, ComputedUserset, User).
+listWR(Namespace, ObjectId, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
+    tuple(Namespace, ObjectId, Tupleset, object(Namespace0, ObjectId0)),
+    list(Namespace0, ObjectId0, ComputedUserset, User).
 
-listWR(Namespace, Id, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
-    tuple(Namespace, Id, Tupleset, userset(Namespace0, Id0, ComputedUserset)),
-    list(Namespace0, Id0, ComputedUserset, User).
+listWR(Namespace, ObjectId, _, User, tupleToUserset(Tupleset, ComputedUserset)) :-
+    tuple(Namespace, ObjectId, Tupleset, userset(Namespace0, ObjectId0, ComputedUserset)),
+    list(Namespace0, ObjectId0, ComputedUserset, User).
 
-listWR(Namespace, Id, Rel, User, union(L, _)) :-
-    listWR(Namespace, Id, Rel, User, L).
+listWR(Namespace, ObjectId, Relation, User, union(L, _)) :-
+    listWR(Namespace, ObjectId, Relation, User, L).
 
-listWR(Namespace, Id, Rel, User, union(_, R)) :-
-    listWR(Namespace, Id, Rel, User, R).
+listWR(Namespace, ObjectId, Relation, User, union(_, R)) :-
+    listWR(Namespace, ObjectId, Relation, User, R).
 
-listWR(Namespace, Id, Rel, User, intersection(L, R)) :-
-    listWR(Namespace, Id, Rel, User, L),
-    listWR(Namespace, Id, Rel, User, R).
+listWR(Namespace, ObjectId, Relation, User, intersection(L, R)) :-
+    listWR(Namespace, ObjectId, Relation, User, L),
+    listWR(Namespace, ObjectId, Relation, User, R).
 
-listWR(Namespace, Id, Rel, User, exclusion(M, S)) :-
-    listWR(Namespace, Id, Rel, User, M),
-    \+ listWR(Namespace, Id, Rel, User, S).
+listWR(Namespace, ObjectId, Relation, User, exclusion(M, S)) :-
+    listWR(Namespace, ObjectId, Relation, User, M),
+    \+ listWR(Namespace, ObjectId, Relation, User, S).
